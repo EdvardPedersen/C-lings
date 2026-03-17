c87be81ee8fe198b555315ea437aa08acb27d5ac
Edvard Pedersen <edvard.pedersen@uit.no>
2026-03-17 21:14:40 +0100

    add braces problem

---
+++ problems/002_braces.c
@@ -0,0 +1,11 @@
+/*
+ * Now we're really cooking with gas! We've got the include done, and
+ * the everything looks good! But there is still an
+ */
+
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello world!\n");
+    return 0;
