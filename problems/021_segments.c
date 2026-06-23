eefa707748ece26278d6efbbad0a9d902935ee4a
BAaboe <benjamin.mjaatvedt@gmail.com>
2026-06-23 10:51:40 +0200

    new problem: segments

---
+++ problems/021_segments.c
@@ -0,0 +1,37 @@
+/* In memory the program is divided into different segments, the most imoprtant
+ * ones are: text, bss, rodata, data, stack.
+ *
+ * The text segment is where the code lives, and is read only memory.
+ *
+ * The bss segment is where all the global/static unitialized variables are,
+ * e.g. `static int a;`, here a will be placed in bss. This segment is both
+ * readable and writeable.
+ *
+ * The rodata segment is where all constants lives, e.g. `static const int a = 1;`,
+ * here gcc will place 'a' in the rodata segment, string literals are also
+ * placed here. Rodata is a read only.
+ *
+ * The data segment is where all the non-constant data defined at compile time
+ * lives, e.g. `static int a = 1`, here gcc will place 'a' into the data
+ * segment. Data is both readable and writeable.
+ *
+ * The stack is a LIFO(Last in First out) storage for non-global/not-static
+ * variables, e.g. `int a[2] = {1,2}`, here gcc will place the array on the
+ * stack. The stack is both readable and writeable.
+ *
+ * It should be noted that segment name and placement is not guaranteed by the C
+ * standard, but on most systems it is as described above.
+ *
+ * This program encounters an error because something is in the wrong segment.
+ *
+ * Hint: Only two character changes are needed.
+ */
+
+#include <stdio.h>
+
+int main() {
+  char *string = "Test Stting";
+  string[7] = 'r';
+  printf("%s\n", string);
+  return 0;
+}
