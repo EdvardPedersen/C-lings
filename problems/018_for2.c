864ca0dfacaa0065a0d6b9695800807c378f9625
BAaboe <benjamin.mjaatvedt@gmail.com>
2026-07-06 12:45:10 +0200

    new probem: for2

---
+++ problems/018_for2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  int start = 0;
+  int end = 10;
+  // Printing every number between start and end, including the end points
+  for (int i = start; i < end; i++) {
+    printf("i = %d\n", i);
+  }
+}
