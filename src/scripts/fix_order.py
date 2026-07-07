2ead08986f98817cf1e214540f4e5d50247f173f
BAaboe <benjamin.mjaatvedt@gmail.com>
2026-07-07 12:42:15 +0200

    fix: fix_order

--- src/scripts/fix_order.py
+++ src/scripts/fix_order.py
@@ -10,8 +10,6 @@ if __name__ == "__main__":
     if len(sys.argv) == 3:
         IN_PROBLEM_DIR = sys.argv[1] + "/" if sys.argv[1][-1] != "/" else ""
         OUT_PROBLEM_DIR = sys.argv[2] + "/" if sys.argv[2][-1] != "/" else ""
-        print(IN_PROBLEM_DIR)
-        print(OUT_PROBLEM_DIR)
     else:
         print("Not the correct number of arguments")
         exit(-1)
