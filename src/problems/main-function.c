365b567a351d4854523a383a51b4ca85e66b29a6
BAaboe <benjamin.mjaatvedt@gmail.com>
2026-07-14 11:55:30 +0200

    new problem: main-function

---
+++ src/problems/main-function.c
@@ -0,0 +1,10 @@
+/*
+ * When returning from the main function, we return 0 if everything went fine,
+ * if not we return something else.
+ */
+#include <stdio.h>
+
+int main() {
+  printf("Hello, World!\n");
+  return -1;
+}
