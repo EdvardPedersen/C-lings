a5b521b1b6d84a3eebe9e2cdf82ddd0328018082
BAaboe <benjamin.mjaatvedt@gmail.com>
2026-07-17 13:24:32 +0200

    new problem: ternary

---
+++ src/problems/ternary.c
@@ -0,0 +1,13 @@
+/*
+ * You can create a oneline if condtion using ternaries. The syntax for ternary
+ * is: 'condtion ? expression if true : expression if false'
+ *
+ * This codes checks if a is one and prints True.
+ */
+#include <stdio.h>
+
+int main() {
+  int a = 1;
+
+  printf("False\n") ? printf("True\n") : a;
+}
