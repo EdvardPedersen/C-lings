13ddadffa28ca65b6273c0ac2cceca3aa569f754
BAaboe <benjamin.mjaatvedt@gmail.com>
2026-07-21 10:51:28 +0200

    remade array2, the old was almost the same as array(1)

--- src/problems/array2.c
+++ src/problems/array2.c
@@ -1,11 +1,27 @@
 /*
- * Indexing in C starts at 0. So the first element of an array has the index 0.
+ * You can initialize an array in C by using curly braces with comma seperated
+ * elements.
  */
-
 #include <stdio.h>
 
+int fibonacci(int number) {
+  int a = 0;
+  int b = 1;
+  if (number < 1)
+    return 0;
+  for (int i = 2; i <= number; i++) {
+    int c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
+
 int main() {
-  int digits_of_pi[] = {1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9};
-  // Printing the 5th digit of pi
-  printf("The 5th digit of pi is: %d\n", digits_of_pi[5]);
+  int numbers[5] = {0, 1, 1, 2, 3, 5, 8, 13, 21};
+
+  numbers[9] = fibonacci(9);
+
+  printf("The 11th number in the fibonacci sequence is: %d\n",
+         numbers[8] + numbers[9]);
 }
