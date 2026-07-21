f1e03b73776868a85d2b59418b0272e5b655f23d
BAaboe <benjamin.mjaatvedt@gmail.com>
2026-07-21 13:34:20 +0200

    new problem: hello2

---
+++ src/problems/hello2.c
@@ -0,0 +1,11 @@
+/*
+ * If we want to print a new line we need to use the the special character '\n'.
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello World
+			This is a new line");
+  return 0;
+}
